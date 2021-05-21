296b67eb34bc06ef9782bc02828e87af204d6fe4
JoeMcGuchan <jmcguchan@gmail.com>
2021-05-21 20:54:25 +0100

    yum yum the commits keep comin

--- make_images.py
+++ make_images.py
@@ -5,26 +5,31 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-cardSideMargins = 18
-titleThickness = 24
-bottomThickness = 48
+scaleFactor = 0.5
+cardHeight = int(1417 * scaleFactor)
+cardWidth = int(827 * scaleFactor)
 
-smallFontSize = 16
-mediumFontSize = 12
-largeFontSize = 24
+cardSideMargins = int(74 * scaleFactor)
+lowerBorderInlay = int(20 * scaleFactor)
+titleThickness = int(84 * scaleFactor)
+bottomThickness = int(265 * scaleFactor)
+
+smallFontSize = int(32 * scaleFactor)
+mediumFontSize = int(36 * scaleFactor)
+largeFontSize = int(48 * scaleFactor)
 
 filename = 'Screenshot.jpg'
 app = QApplication(sys.argv)
 
-fontId = QFontDatabase.addApplicationFont("fonts/Austie Bost Roman Holiday Sketch.ttf");
+fontId = QFontDatabase.addApplicationFont("../fonts/Austie Bost Roman Holiday Sketch.ttf");
 fontFamily = QFontDatabase.applicationFontFamilies(fontId)[0];
 
 fontSmall = QFont(fontFamily, smallFontSize)
 fontMedium = QFont(fontFamily, mediumFontSize)
 fontLarge = QFont(fontFamily, largeFontSize)
 
-player_card_csv = "card_csvs/player_cards.csv"	
-gm_card_csv = "card_csvs/gm_cards.csv"	
+player_card_csv = "../card_csvs/player_cards.csv"	
+gm_card_csv = "../card_csvs/gm_cards.csv"	
 
 class Card:
 	background = ""
@@ -50,7 +55,7 @@ class Card:
 
 		wholeCard = QLabel()
 		wholeCard.setPixmap(backgroundPixmap)
-		wholeCard.resize(360, 661)
+		wholeCard.resize(cardWidth, cardHeight)
 		wholeCard.setScaledContents(True)
 		wholeCard.setLayout(QGridLayout())
 		wholeCard.layout().setContentsMargins(cardSideMargins,cardSideMargins,cardSideMargins,cardSideMargins)
@@ -74,12 +79,11 @@ class Card:
 		extraLabel.setFont(fontMedium)
 
 		skullPixmap = QPixmap()
-		skullPixmap.load("card_templates/skull_icon.png")
+		skullPixmap.load("../card_templates/skull_icon.png")
 
 		skullLabel = QLabel()
 		skullLabel.setVisible(self.lethal)
-		skullLabel.setPixmap(skullPixmap)
-		skullLabel.resize(52, 52)
+		skullLabel.setPixmap(skullPixmap.scaled(titleThickness,titleThickness))
 		skullLabel.setScaledContents(True)
 
 		if self.hasTopLine():
@@ -97,41 +101,33 @@ class Card:
 		wholeCard.layout().setRowStretch(1,1)
 		wholeCard.layout().setColumnStretch(1,1)
 
-		wholeCard.layout().addWidget(titleLabel, 0, 0, 1, 3, Qt.AlignHCenter | Qt.AlignVCenter)
-		wholeCard.layout().addWidget(descriptionLabel, 2, 0, 1, 3, Qt.AlignCenter | Qt.AlignVCenter)
-		wholeCard.layout().addWidget(difficultyLabel, 3, 0, 1, 1, Qt.AlignLeft | Qt.AlignBottom)
-		wholeCard.layout().addWidget(extraLabel, 3, 1, 1, 1, Qt.AlignRight | Qt.AlignBottom)
-		wholeCard.layout().addWidget(skullLabel, 3, 2, 1, 1, Qt.AlignLeft | Qt.AlignBottom)
+		wholeCard.layout().addWidget(titleLabel, 0, 0, 1, 5, Qt.AlignHCenter | Qt.AlignVCenter)
+		wholeCard.layout().addWidget(descriptionLabel, 2, 0, 1, 5, Qt.AlignCenter | Qt.AlignVCenter)
+		wholeCard.layout().addWidget(difficultyLabel, 3, 1, 1, 1, Qt.AlignLeft | Qt.AlignBottom)
+		wholeCard.layout().addWidget(extraLabel, 3, 2, 1, 1, Qt.AlignRight | Qt.AlignBottom)
+		wholeCard.layout().addWidget(skullLabel, 3, 3, 1, 1, Qt.AlignLeft | Qt.AlignVCenter)
 
-		wholeCard.setFrameShape(QFrame.Box)
-		titleLabel.setFrameShape(QFrame.Box)
-		descriptionLabel.setFrameShape(QFrame.Box)
-		difficultyLabel.setFrameShape(QFrame.Box)
-		extraLabel.setFrameShape(QFrame.Box)
-		skullLabel.setFrameShape(QFrame.Box)
+		wholeCard.layout().setColumnMinimumWidth(0, lowerBorderInlay)
+		wholeCard.layout().setColumnMinimumWidth(4, lowerBorderInlay)
 
 		wholeCard.show()
 
 		app.exec_()
 		wholeCard.grab().save(self.saveLocation + "/" + self.title + ".png")
 
-def makePlayerCard(cardInfo):
-	card = Card()
-	return card
-
 def makeGateCard(cardInfo, app):
 	card = Card()
 
-	card.background = "card_templates/gm_cards/the_gate.png"
-	card.saveLocation = "produced_cards/gm_cards"
+	card.background = "../card_templates/gm_cards/the_gate.png"
+	card.saveLocation = "../produced_cards/gm_cards"
 
 	card.draw(app)
 
 def makeOpenCard(cardInfo, app):
 	card = Card()
 
-	card.background = "card_templates/gm_cards/open.png"
-	card.saveLocation = "produced_cards/gm_cards/open"
+	card.background = "../card_templates/gm_cards/open.png"
+	card.saveLocation = "../produced_cards/gm_cards/open"
 	card.title = cardInfo["NAME"]
 	card.description = cardInfo["TEXT"]
 	card.difficulty = cardInfo["DIFFICULTY"]
@@ -145,8 +141,8 @@ def makeOpenCard(cardInfo, app):
 def makeThreatCard(cardInfo, app):
 	card = Card()
 
-	card.background = "card_templates/gm_cards/threat.png"
-	card.saveLocation = "produced_cards/gm_cards/threat"
+	card.background = "../card_templates/gm_cards/threat.png"
+	card.saveLocation = "../produced_cards/gm_cards/threat"
 	card.title = cardInfo["NAME"]
 	card.description = cardInfo["TEXT"]
 	card.difficulty = cardInfo["DIFFICULTY"]
@@ -176,6 +172,101 @@ def makeGMCard(cardInfo, app):
 	if (typeText == "THREAT"):
 		makeThreatCard(cardInfo, app)
 		return
+
+def makeTowerCard(cardInfo, app):
+	card = Card()
+
+	card.background = "../card_templates/player_cards/the_tower.png"
+	card.saveLocation = "../produced_cards/player_cards"
+
+	card.draw(app)
+
+def makeTraitCard(cardInfo, app):
+	card = Card()
+
+	subType = cardInfo["SUBTYPE"].lower()
+
+	card.background = "../card_templates/player_cards/traits/"+subType+".png"
+	card.saveLocation = "../produced_cards/player_cards/traits/"+ subType
+	card.title = cardInfo["NAME"]
+	card.description = cardInfo["TEXT"]
+
+	card.draw(app)
+
+def makeAdvancedTraitCard(cardInfo, app):
+	card = Card()
+
+	subType = cardInfo["SUBTYPE"].lower()
+
+	card.background = "../card_templates/player_cards/skills/"+subType+".png"
+	card.saveLocation = "../produced_cards/player_cards/skills/"+ subType
+	card.title = cardInfo["NAME"]
+	card.description = cardInfo["TEXT"]
+
+	card.draw(app)
+
+def makeWoundCard(cardInfo, app):
+	card = Card()
+
+	subType = cardInfo["SUBTYPE"].lower()
+
+	card.background = "../card_templates/player_cards/minor_wounds/"+subType+".png"
+	card.saveLocation = "../produced_cards/player_cards/"+subType+"/minor_wounds"
+	card.title = cardInfo["NAME"]
+	card.description = cardInfo["TEXT"]
+
+	card.draw(app)
+
+def makeMajorWoundCard(cardInfo, app):
+	card = Card()
+
+	subType = cardInfo["SUBTYPE"].lower()
+
+	card.background = "../card_templates/player_cards/minor_wounds/"+subType+".png"
+	card.saveLocation = "../produced_cards/player_cards/"+subType+"/major_wounds"
+	card.title = cardInfo["NAME"]
+	card.description = cardInfo["TEXT"]
+
+	card.draw(app)
+
+def makeBlessingCard(cardInfo, app):
+	card = Card()
+
+	subType = cardInfo["SUBTYPE"].lower()
+
+	card.background = "../card_templates/player_cards/skills/"+subType+".png"
+	card.saveLocation = "../produced_cards/player_cards/skills"
+	card.title = cardInfo["NAME"]
+	card.description = cardInfo["TEXT"]
+
+	card.draw(app)
+
+def makePlayerCard(cardInfo, app):
+	typeText = cardInfo["TYPE"]
+
+	if (typeText == "THE TOWER"):
+		makeTowerCard(cardInfo, app)
+		return
+
+	if (typeText == "TRAIT"):
+		makeTraitCard(cardInfo, app)
+		return
+
+	if (typeText == "ADVANCED TRAIT"):
+		makeAdvancedTraitCard(cardInfo, app)
+		return
+
+	if (typeText == "WOUND"):
+		makeWoundCard(cardInfo, app)
+		return
+
+	if (typeText == "MAJOR WOUND"):
+		makeMajorWoundCard(cardInfo, app)
+		return	
+
+	if (typeText == "BLESSING"):
+		makeBlessingCard(cardInfo, app)
+		return	
 
 with open(gm_card_csv, newline='') as csvItems:
     itemsReader = csv.DictReader(csvItems, delimiter=',', quotechar='"')
